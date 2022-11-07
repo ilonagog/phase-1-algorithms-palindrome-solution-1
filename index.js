58ff01bf173a9ef6530e692cb67f66ff7fc1c9af
ilona <gogiashvili.ilona@gmail.com>
2022-11-07 09:23:58 -0500

    commit tree f724b2b1ed8b0bf60bc5b1ad314038e864d98fa9

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-function isPalindrome(word) {
+function reverseString(word) {
   // Write your algorithm here
+  //convert string to an array 
+  const wordArray = word.split("")
+  //reverse the array values
+  //const reverseWordValues = wordValues.reverse();
+  //convert array to string 
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join('');
+
+  return reversedWord;
 }
 
+
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// if (string == reverseString) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+
+
+
+
+
+
+
+
+//reverseString();
 /* 
   Add your pseudocode here
+
+
 */
 
 /*
   Add written explanation of your solution here
+
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +58,20 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("nun"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("hannah"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("story"));
+
+  console.log("");
+
 }
 
 module.exports = isPalindrome;
